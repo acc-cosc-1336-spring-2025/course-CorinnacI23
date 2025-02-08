61bfef3263601104cf508f2177c3ae35c993a57b
CorinnacI23 <corinna.clark@g.austincc.edu>
2025-02-08 00:14:37 +0000

    Homework 2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,6 @@
-#
+
+import unittest
+from src.homework.c_decisions.decisions import get_options_ratio
+from src.homework.c_decisions.decisions import get_faculty_rating
+
+class Test_Config(unittest.TestCase):
