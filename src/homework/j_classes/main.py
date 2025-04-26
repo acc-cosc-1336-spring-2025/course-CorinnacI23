2998afe24599e0e84cc1f7698390f3ae48050127
CorinnacI23 <corinna.clark@g.austincc.edu>
2025-04-26 22:24:37 +0000

    Homework 9

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -1,1 +1,18 @@
-#
+from class_a import Die
+
+def main():
+    die = Die()
+    while True:
+        user_input = input("Roll the die? (y/n): ").lower()
+        if user_input == 'y':
+            die.roll()
+            print(die)
+        elif user_input == 'n':
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid input. Please enter 'y' or 'n'.")
+
+if __name__ == "__main__":
+    main()
+
